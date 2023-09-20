8945a22cb7622407cdf14cda43491e7accca9852
Bruno <bruno.if@hotmail.com>
2023-09-20 11:14:21 -0300

    aula 8

---
+++ aula08.py
@@ -0,0 +1,15 @@
+aula = True
+condicional = 10 > 5 #True
+
+print(aula)
+
+#É possível converter str para bool
+texto = 'tem conteudo'
+numero = 100
+
+print(bool(texto)) #Tem conteúdo e portanto retorna True, strings vazias retornam False
+print(bool(numero)) #Valores diferentes de 0 retorna True, se for 0 retorna False
+
+
+
+
