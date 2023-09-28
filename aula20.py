b527d43597281b1dabc559b68e770357722f4f7c
Bruno Santos <bruno.if@hotmail.com>
2023-09-28 12:23:29 -0300

    aula 21

--- aula20.py
+++ aula20.py
@@ -1,1 +1,31 @@
 #Funções com argumentos
+
+def somarComParametros(num1, num2):
+  print('Soma de 2 números = ' + str(num1 + num2))
+
+somarComParametros(10, 20)
+
+#Funções com argumentos arbitrados
+
+def textos(*txt):
+  for t in txt:
+    print(t)
+
+textos('texto1', 'texto2', 'texto3', 'texto4', 'texto5')
+
+#Funções com argumentos arbitrados devem estar no final
+
+def numeros(n1, *outrosNumeros):
+  print(n1,"Esse é o primeiro")
+  for outros in outrosNumeros:
+    print(outros)
+
+numeros(1, 2, 3, 4, 5)
+
+#Funções com valores padrão
+
+def carros(modelo = "Gol"):
+  print("modelo: ", modelo)
+
+carros("HB20")
+carros()
---
+++ aula21.py
@@ -0,0 +1,9 @@
+valores = [1, 3, 5, 7]
+
+def somar(num):
+    r = 0
+    for n in num:
+        r += n
+    return r
+
+print(somar(valores))
